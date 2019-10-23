32f07c104f84f01bf02a8f81e530a64cad5ddf30
Federico Mariño <fede.m.fede@gmail.com>
2019-10-23 07:51:01 -0300

    added excercises for parameters and return

--- 02 - Functions/ejercicios-parameters.js
+++ 02 - Functions/ejercicios-parameters.js
@@ -1,3 +1,33 @@
+// 1
 /***********************************************
  * 
+ * Escribir una función que reciba como parámetro
+ * un número e imprima ese número al revés.
+ * Pista: Buscar como transformar los strings en arrays.
+ * 
+ ***********************************************/
+
+ //2
+ /***********************************************
+ * 
+ * Escribir una función que reciba una palabra
+ * e imprima sus letras ordenadas alfabéticamente.
+ *  
+ ***********************************************/
+
+// 3
+ /***********************************************
+ * 
+ * Escribir una función que reciba una frase
+ * e imprima la misma con la primera letra de 
+ * cada palabra en mayúsculas.
+ *  
+ ***********************************************/
+
+ //4
+/***********************************************
+ * 
+ * Escribir una función que reciba una frase
+ * e imprima la palabra mas larga de la misma
+ *  
  ***********************************************/

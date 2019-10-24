508063a5ae4743a0956c58ad2f5cb7b25c991547
Federico Marino <fede.m.fede@gmail.com>
2019-10-24 09:15:02 -0300

    added excercises for return. Added integral excercise

---
+++ 02 - Functions/ejercicio-integral.js
@@ -0,0 +1,17 @@
+/***********************************************
+ * 
+ * 1. Hacer una función que guarde una lista
+ *    de listas de datos de personas. Deberia
+ *    guardar por cada persona, una lista de datos
+ *    que incluyan el nombre completo, la edad y 
+ *    la dirección de email.
+ * 2. Hacer una función que me devuelva la lista
+ *    completa de personas.
+ * 3. Hacer una función que me permita encontrar
+ *    una persona por email.
+ * 4. Hacer una función que me permita encontrar 
+ *    personas por nombre o parte del nombre.
+ * 
+ * Observaciones: Google. Mucho google.
+ * 
+ ************************************************/
--- 02 - Functions/ejercicios-return.js
+++ 02 - Functions/ejercicios-return.js
@@ -5,3 +5,14 @@
  * el número de vocales que tiene la misma.
  *  
  ***********************************************/
+
+ // 2
+ /***********************************************
+ * 
+ * Hacer una función que tome, primero, una frase
+ * y luego pida un caracter para remover de la 
+ * frase ingresada.
+ * Observaciones: validar que el caracter ingresado
+ * sea solo uno. Si es más de uno tirar un error.
+ *  
+ ***********************************************/

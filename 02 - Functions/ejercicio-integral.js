3c2cdc32433c4235ad4ea7900a265a146f2c2fe6
Federico Marino <fede.m.fede@gmail.com>
2019-10-29 10:23:56 -0300

    Agregadas nuevas consignas

--- 02 - Functions/ejercicio-integral.js
+++ 02 - Functions/ejercicio-integral.js
@@ -5,13 +5,21 @@
  *    guardar por cada persona, una lista de datos
  *    que incluyan el nombre completo, la edad y 
  *    la dirección de email.
+ * 1b. Al ingresar nueva persona, validar que no 
+ *     exista en la lista. Si existe, tirar
+ *     un error. (Usar funcion distinta para validar
+ *     y llamar dentro de la funcion de guardar persona).
  * 2. Hacer una función que me devuelva la lista
  *    completa de personas.
  * 3. Hacer una función que me permita encontrar
  *    una persona por email.
  * 4. Hacer una función que me permita encontrar 
  *    personas por nombre o parte del nombre.
- * 
+ * 5. Hacer una función para borrar personas por mail.
+ * 6. Hacer una funcion que permita modificar nombre
+ *    buscando por mail.
+ * 7. Hacer una funcion que permita modificar edad 
+ *    buscando por mail.
  * Observaciones: Google. Mucho google.
  * 
  ************************************************/

7c2a8399c586713eeb5e6169df0122eb6191d642
Federico Marino <fede.m.fede@gmail.com>
2019-10-30 18:32:04 -0300

    added excercises for tdd

---
+++ 03 - Test unitarios/ejercicios.js
@@ -0,0 +1,15 @@
+/***************************************************
+ * Desarrollar una función que reciba por parámetro
+ * un día de la semana y devuelva el número que 
+ * le corresponde.
+ * Comenzar por los casos de test, y luego hacer 
+ * la función.
+ */
+
+ /**************************************************
+  * Desarrollar una función que convierta
+  * de minutos a segundos. Pasar minutos por parámetro
+  * y devolver los segundos.
+  * Comenzar por los casos de test, y luego hacer la 
+  * función. Recordar tirar errores y testearlos.
+  */

2a510159e950aecf89460b47dfbcb662031c6725
Federico Marino <fede.m.fede@gmail.com>
2019-11-03 16:31:28 -0300

    nuevo ejercicio integrador en tests unitarios

--- 03 - Test unitarios/ejercicios.js
+++ 03 - Test unitarios/ejercicios.js
@@ -13,3 +13,18 @@
   * Comenzar por los casos de test, y luego hacer la 
   * función. Recordar tirar errores y testearlos.
   */
+
+/**************************************************
+ * Hacer una serie de funciones para un TODO list.
+ * Que me permita cargar una tarea, con un titulo, 
+ * descripcion de tarea, y si fue hecha o no, con un 
+ * valor default.
+ * Me tiene que permitir agregar tareas,
+ * editar y eliminar. Además debería poder listar
+ * las tareas permitiendome filtrar por
+ * si fueron resueltas o no, pero sin ser obligatorio.
+ * Por cada funcion que haga, primero empiezo por 
+ * el test, y luego por la funcion.
+ * Observaciones: Pensar en ejercicio integrador de 
+ * cargar personas.
+ */

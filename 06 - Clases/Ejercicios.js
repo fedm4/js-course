72d55ec0e15ac628e360a9a584e79e27d1f34411
Federico Marino <fede.m.fede@gmail.com>
2019-11-21 09:37:34 -0300

    w

--- 06 - Clases/Ejercicios.js
+++ 06 - Clases/Ejercicios.js
@@ -18,3 +18,40 @@
   * del ejercicio anterior a nuevas clases
   * que extiendan de la clase principal.
   */
+
+/**
+ * Refactorizar la lista de productos con el 
+ * carrito de compras usando clases. 
+ * Vamos a tener 3 tipos de productos, 
+ *  1. Perfumes
+ *  2. Chocolates
+ *  3. Auriculares
+ * Los 3 productos van a tener distintas 
+ * caracteristicas, aunque compartan marca y precio.
+ * Los chocolates tienen porcentaje, leche, tipo, 
+ * extras (almendras, mani, pasas). 
+ * Los auriculares van a tener una variable
+ * "wireless" y una "tipo" (cerrados, abiertos)
+ * 
+ * Voy a necesitar un metodo que me devuelva una lista
+ * formateada con todos los productos y caracteristicas 
+ * en un texto. Uno por linea.
+ * 
+ * Ademas de eso cada producto puede tener descuentos, en 
+ * porcentaje.
+ * 
+ * Hay que refactorizar el carrito de compras, de forma
+ * que tenga lo mismo que hacia antes, pero a la hora de 
+ * calcular el precio, lo haga teniendo en cuenta los
+ * descuentos que aplican a cada producto.
+ * 
+ * El carrito tiene que tener una funcion que me permita
+ * listar todos los productos agregados formateados con 
+ * descripcion, cantidad, y un producto por linea.
+ * 
+ * Por ultimo sumar una clase cupon que tiene otro descuento
+ * en porcentaje. Yo puedo aplicar un solo cupon a mi carrito
+ * de compras y el calculo final del precio tiene que tomar
+ * en cuenta esto.
+ * 
+ */

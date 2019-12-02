c6e4f9aa411719d9cdff5e11726f958db0eb91b8
Federico Marino <fede.m.fede@gmail.com>
2019-12-02 20:35:48 -0300

    ejercicios estilos

--- 07 - DOM/02 - Ejercicios.js
+++ 07 - DOM/02 - Ejercicios.js
@@ -1,17 +1,18 @@
 //01
 /**
  * Crear un input de tipo checkbox.
- * Escribir una funcion que cambie el estado de 
- * clickeado a sin clickear o viceversa.
+ * Escribir una funcion "toggleChecked" que 
+ * cambie el estado de clickeado a sin 
+ * clickear o viceversa.
  */
 
  // 02
  /**
   * Dado el siguiente HTML
   */
- <div id="miDiv" name="miDiv"></div>
+ <input type="text" id="miInput" name="miInput"></input>
 /**
- * cambiar el atributo name a miExDiv
+ * cambiar el atributo name a miExInput
  */
 
  // 03
@@ -19,4 +20,22 @@
   * Escribir un HTML con un div.
   * Luego, por ID, modificar el contenido del DIV
   * agregando una lista de 5 elementos.
+  * Una vez agregada la lista, agregar 
+  * programaticamente un numero a cada elemento
+  * 
+  * por ejemplo, si son 5 elementos deberia verse:
+  * 1
+  * 2
+  * 3
+  * 4
+  * 5
   */
+
+  //04
+  /**
+   * Escribir una funcion que se llame changeTitle.
+   * Para esto deberemos tener dos elementos,
+   * un input, y un h1. 
+   * Debemos tomar el valor del input y ponerlo como 
+   * el title. Si ya existe texto, reemplazarlo.
+   */
---
+++ 07 - DOM/03 - Ejercicios.js
@@ -0,0 +1,21 @@
+// 001
+/**
+ * Escribir una funcion que nos permita pasarle un elemento 
+ * y un color, y  le cambie el color de fondo al elemento.
+ */
+
+ //002 
+ /**
+  * Modificar la funcion previa para que, si no se recibe
+  * un color por parametro, se busque un color de forma
+  * azarosamente.
+  */
+
+  // 003
+  /**
+   * Crear una lista con varios elementos.
+   * Crear una funcion que asigne la clase
+   * "active" a uno de estos elementos seleccionados.
+   * Ademas al mismo tiempo tiene que eliminar
+   * la clase "active" del resto de los elementos.
+   */

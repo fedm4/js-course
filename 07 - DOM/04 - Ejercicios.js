709d216a568b33c7ab17d4945a41a906ed74ecd7
Federico Marino <fede.m.fede@gmail.com>
2019-12-12 08:12:56 -0300

    restaurant

--- 07 - DOM/04 - Ejercicios.js
+++ 07 - DOM/04 - Ejercicios.js
@@ -26,13 +26,22 @@
   * Para esto necesitamos.
   * 1. Lista de mesas - agregar, eliminar
   * 2. Lista de productos con precio - Agregar, eliminar, modificar
-  * 3. Lista de mozos - Agregar, eliminar
   * 
   * Y que nos permita obtener
-  * 4. Una cuenta por mesa (la mesa se abre, se cargan los productos, y 
+  * 3. Una cuenta por mesa (la mesa se abre, se cargan los productos, y 
   * se cierra pidiendo el total consumido. Lista de cuentas con objetos)
-  * 5. Obtener el producto mas vendido
+  * 4. Obtener el producto mas vendido
+  * 
+  * Las funciones deben tener validaciones correspondientes.
   * 
   * Y esto debe estar todo accesible desde una pagina web.
   * TESTS de las funciones que no requieran DOM
+  * 
+  * En el GUI (graphical user interface, o interfaz grafica de usuario), o sea
+  * la pagina, vamos a tener varias acciones relacionadas a inputs.
+  * Resetear el valor de los inputs cada vez que hago click en el boton que le 
+  * corresponde al input. O sea, si agrego un nuevo numero de mesa, el input
+  * debe quedar vacio una vez agregado.
+  * 
+  * Por ultimo, puede hacerse con objetos o con clases.
   */

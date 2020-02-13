bd8acaeb2bb4a3867fdcd1159439e7e1810131ab
Federico Marino <fede.m.fede@gmail.com>
2020-02-13 08:11:10 -0300

    hooks custom ejercicios

---
+++ 09 - React/Ejercicios.js
@@ -0,0 +1,13 @@
+/**
+ * Hacer un hook que me permita manejar un booleano
+ * Este hook tiene que devolverme el valor booleano
+ * y una funcion que se llame toggleValue. La funcion
+ * debe cambiar al estado contrario. El hook debe tomar
+ * como parametro el primer valor que queremos para nuestro
+ * booleano.
+ * Tambien debera ser aplicado a dos componentes
+ * 1. Un componente que, con un boton, me muestre una 
+ * imagen o la oculte.
+ * 2. Otro componente con un checkbox y un input de texto. 
+ * El checkbox debe permitirme deshabilitar el input.
+ */

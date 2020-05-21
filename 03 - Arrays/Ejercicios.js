d17d94235bf3baddc7efea62474533182d889414
Federico Mariño <fede.m.fede@gmail.com>
2020-05-21 20:05:00 -0300

    arrays ejercicios

--- 03 - Arrays/Ejercicios.js
+++ 03 - Arrays/Ejercicios.js
@@ -8,3 +8,10 @@
  * ¡Hola Grace!
  * */
 
+//02
+/**
+ * Crear un programa que pida ingresar números separados por espacios y devuelva un mensaje con el menor número ingresado. Ejemplo:
+ * 
+ * Ingrese números: 5 7 99 34 54 2 12
+ * El menor número es: 2
+ */

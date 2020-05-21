f4f7f3adc88514b3a4be0bb19bd59ee592185e7f
Federico Mariño <fede.m.fede@gmail.com>
2020-05-21 20:49:59 -0300

    Mas ejercicios de arrays

--- 03 - Arrays/Ejercicios.js
+++ 03 - Arrays/Ejercicios.js
@@ -15,3 +15,43 @@
  * Ingrese números: 5 7 99 34 54 2 12
  * El menor número es: 2
  */
+
+ // 03
+ /**
+  * Crear un programa que pida ingresar números separados por espacios y devuelva un mensaje con la suma de todos los números. Ejemplo:
+  * Ingrese números: 5 7 10 12 24
+  * La suma de todos los números es: 58
+  */
+
+  // 04
+/**
+ * Crear un programa que permite ingresar valores separados por espacios, y luego preguntar qué valor se desea averiguar si existe. El programa debe devolver un mensaje diciendo si dicho valor se encuentra dentro de los valores originales o no. Ejemplo:
+ * Ingrese valores: 5 7 99 34 54 2 12
+ * Ingrese valor a buscar: 54
+ * El valor 54 se encuentra entre los valores originales
+ */
+
+// 05
+/**
+ * Crear un programa que permite ingresar valores separados por espacios, y luego preguntar qué valor se desea encontrar el índice. El programa debe devolver un mensaje con el índice del primer elemento que sea igual al buscado, o indicar si no se ha encontrado. Ejemplo:
+ * Ingrese valores: 5 7 12 34 54 2 12
+ * Ingrese valor a buscar: 12
+ * El valor 12 se encuentra en el índice 12
+ */
+
+ // 06
+ /**
+  * Crear un programa que permita saber si Sam y Frodo están juntos. El programa debe permitir ingresar nombres separados por espacios, y mostrar con un mensaje si Sam se encuentra al lado de Frodo, ya sea antes o después. Ejemplo:
+  * Ingresar nombres: Sam Frodo Legolas
+  * Sam y Frodo están juntos, ¡Frodo está a salvo!
+  * Ingresar nombres: Sam Orco Frodo
+  * Sam y Frodo están separados, ¡Frodo está en peligro!
+  */
+
+// 07
+/**
+ * Crear un programa que pida ingresar "manzana", "pera"y "durazno" y devuelva un mensaje 
+ * con la cantidad que hay de cada una. Ejemplo:
+ * Ingrese frutas: manzana manzana pera durazno pera durazno manzana
+ * Hay 3 🍎, 2 🍐 y 2 🍑
+ */

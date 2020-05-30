c8a7f2b458aef63741f1669d3c6ea2ae349347f7
Federico Mariño <fede.m.fede@gmail.com>
2020-05-30 13:37:03 -0300

    /

--- 04 - Functions/04 - Callbacks.js
+++ 04 - Functions/04 - Callbacks.js
@@ -5,12 +5,25 @@
  */
 
  const miFuncion1 = callback => {
-    miFuncion2();
+    console.log("Hago algo");
+    callback();
  };
 
- const miFunction2 = callback => console.log("Estoy en funcion 2");
+ miFuncion1(() => console.log("Estoy en funcion 2"));
 
- miFuncion1();
+ // Es lo mismo que hacer
+const func2 = () => console.log("Estoy en funcion 2");
+
+miFuncion1(func2);
+ 
+ // Es lo mismo que hacer
+ miFuncion1(() => {
+    return console.log("Estoy en funcion 2");
+ });
+
+ // Yo en los callbacks paso una defincion de una funcion
+ // Que se puede ejecutar desde dentro de la funcion a la 
+ // que lo pase
 
  /**
   * Estas funciones que se pasan por parametro se llaman Callbacks
@@ -23,7 +36,7 @@
  // el item de cada iteracion.
  // Paso un callback que a su vez recibe por parametro el item del array
  // y el indice que le corresponde.
- forEach((item, index) => {
+ [1,2,3].forEach((item, index) => {
     console.log(item, index); // hago algo con este codigo.
  });
 
@@ -41,15 +54,19 @@
  // map transforma el array y me devuelve un array con la misma cantidad de elementos
  // que el original
  const nuevoArr = [1,2,3].map(item => item + 1);
+ // es lo mismo que 
+ const nuevoArr = [1,2,3].map((item) => {
+    return item + 1;
+ })
  console.log(nuevoArr)// [2,3,4]
 
  // filter
- const nuevoArr = [1,2,3,4].map(item => item > 2); // notese que el return es una comparacion / return item > 2. 
+ const nuevoArr = [1,2,3,4].filter(item => item > 2); // notese que el return es una comparacion / return item > 2. 
                                                    // Si es true, me sirve, si es  false, lo descarto.
  console.log(nuevoArr); // [3,4]
 
  // find
- const item = [1,2,3,4].map(item => item === 3); // devuelvo true cuando encuentro mi elemento
+ const item = [1,2,3,4].find(item => item === 3); // devuelvo true cuando encuentro mi elemento
 console.log(item) // 3
 
 // find index

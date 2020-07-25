9ab9cad2e51b214fbe16947812e904366a0f8413
Federico Marino <fede.m.fede@gmail.com>
2020-07-25 12:30:19 -0300

    Modificacion ejer dom

--- 09 - DOM/01 - Ejercicios Dom.js
+++ 09 - DOM/01 - Ejercicios Dom.js
@@ -22,13 +22,13 @@
     <span></span>
     <span></span>
     <ul>
-        <li></li>
-        <li></li>
+        <li>element 1</li>
+        <li>element 2</li>
     </ul>
     <span id="spanId"></span>
     <span></span>
  </div>
  /**
-  * Obtener el array de todos los elementos dentro del <div>
+  * Obtener el lista de todos los elementos dentro del <div>
   * usando unicamente selector por ID
   */
